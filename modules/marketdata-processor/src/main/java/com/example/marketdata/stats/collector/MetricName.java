dfa436c0d477571d7dcb0437bc6dd895c3e08c4c
Jorge Cordero <iorch.cordero@gmail.com>
2026-03-14 16:34:51 +0100

    refactor

--- modules/marketdata-processor/src/main/java/com/example/marketdata/stats/collector/MetricName.java
+++ modules/marketdata-processor/src/main/java/com/example/marketdata/stats/collector/MetricName.java
@@ -62,17 +62,17 @@ public final class MetricName {
     public static final String DISPATCHED_EVENTS = DISPATCHED_PREFIX + METRIC_EVENTS;
 
     // Dispatched stage - ZMQ
-    public static final String DISPATCHED_ZMQ_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_ZMQ + "max" + "." + MILLISECONDS;;
+    public static final String DISPATCHED_ZMQ_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_ZMQ + "max" + "." + MILLISECONDS;
     public static final String DISPATCHED_ZMQ_AVG_MS = DISPATCHED_PREFIX + DISPATCHER_ZMQ + "avg" + "." + MILLISECONDS;
     public static final String DISPATCHED_ZMQ_EVENTS_DROPPED = DISPATCHED_PREFIX + DISPATCHER_ZMQ + "." + METRIC_EVENTS_DROPPED;
 
     // Dispatched stage - Hazelcast
-    public static final String DISPATCHED_HAZELCAST_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_HAZELCAST + "max" + "." + MILLISECONDS;;
+    public static final String DISPATCHED_HAZELCAST_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_HAZELCAST + "max" + "." + MILLISECONDS;
     public static final String DISPATCHED_HAZELCAST_AVG_MS = DISPATCHED_PREFIX + DISPATCHER_HAZELCAST + "avg" + "." + MILLISECONDS;
     public static final String DISPATCHED_HAZELCAST_EVENTS_DROPPED = DISPATCHED_PREFIX + DISPATCHER_HAZELCAST + "." + METRIC_EVENTS_DROPPED;
 
     // Dispatched stage - Kafka
-    public static final String DISPATCHED_KAFKA_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_KAFKA + "max" + "." + MILLISECONDS;;
+    public static final String DISPATCHED_KAFKA_MAX_MS = DISPATCHED_PREFIX + DISPATCHER_KAFKA + "max" + "." + MILLISECONDS;
     public static final String DISPATCHED_KAFKA_AVG_MS = DISPATCHED_PREFIX + DISPATCHER_KAFKA + "avg" + "." + MILLISECONDS;
     public static final String DISPATCHED_KAFKA_EVENTS_DROPPED = DISPATCHED_PREFIX + DISPATCHER_KAFKA + "." + METRIC_EVENTS_DROPPED;
 
@@ -80,12 +80,12 @@ public final class MetricName {
     public static final String STORAGE_EVENTS = STORAGE_PREFIX + METRIC_EVENTS;
 
     // Storage stage - Postgres
-    public static final String STORAGE_POSTGRES_MAX_MS = STORAGE_PREFIX + STORAGE_POSTGRES + "max" + "." + MILLISECONDS;;
+    public static final String STORAGE_POSTGRES_MAX_MS = STORAGE_PREFIX + STORAGE_POSTGRES + "max" + "." + MILLISECONDS;
     public static final String STORAGE_POSTGRES_AVG_MS = STORAGE_PREFIX + STORAGE_POSTGRES + "avg" + "." + MILLISECONDS;
     public static final String STORAGE_POSTGRES_EVENTS_DROPPED = STORAGE_PREFIX + STORAGE_POSTGRES + "." + METRIC_EVENTS_DROPPED;
 
     // Storage stage - Oracle
-    public static final String STORAGE_ORACLE_MAX_MS = STORAGE_PREFIX + STORAGE_ORACLE + "max" + "." + MILLISECONDS;;
+    public static final String STORAGE_ORACLE_MAX_MS = STORAGE_PREFIX + STORAGE_ORACLE + "max" + "." + MILLISECONDS;
     public static final String STORAGE_ORACLE_AVG_MS = STORAGE_PREFIX + STORAGE_ORACLE + "avg" + "." + MILLISECONDS;
     public static final String STORAGE_ORACLE_EVENTS_DROPPED = STORAGE_PREFIX + STORAGE_ORACLE + "." + METRIC_EVENTS_DROPPED;
 }
